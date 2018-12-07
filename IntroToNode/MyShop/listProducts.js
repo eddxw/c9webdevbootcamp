5198fa3eb0139dda29c9fade60c159e40022daaf
eddxw <eduardo.jemb@gmail.com>
2018-12-07 12:17:16 +0000

    listProducts faker.fake()

--- IntroToNode/MyShop/listProducts.js
+++ IntroToNode/MyShop/listProducts.js
@@ -4,3 +4,8 @@ for (let i = 0; i < 10; i++) {
     let str = `${faker.commerce.productName()} - $${faker.commerce.price()}`
     console.log(str)
 }
+
+for (let i = 0; i < 10; i++) {
+    console.log(faker.fake(`{{commerce.productName}} - {{commerce.price}}`))
+}
+
