1502d320f4b7447a05e2ca8c84314b1b5cb2d87b
eddxw <eduardo.jemb@gmail.com>
2018-12-09 12:45:50 +0000

    PostRequestMemo

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -4,17 +4,30 @@ var app = express();
 // "/" => "Hi there!"
 app.get("/", function(req, res) {
     res.send("Hi there!");
-})
+});
 
 // "/bye" => "Goodbye!"
 app.get("/bye", function(req, res) {
     res.send("Goodbye!");
-})
+});
 // "/dog" => "MEOW!"
 app.get("/dog", function(req, res) {
     res.send("MEOW!");
-})
+});
 
+app.get("/r/:subredditName", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send(`Welcome to the ${subreddit} subreddit!`);
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+    res.send("Welcome to the comments page!");
+});
+
+///////////////////////
+app.get("*", function(req, res) {
+    res.send("You are a star!");
+});
 
 //
 app.listen(process.env.PORT, process.env.IP, () => {
