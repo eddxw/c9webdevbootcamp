a41d1a1a9e7ea4b7e83d7ec1897ceadabd63c630
eddxw <eduardo.jemb@gmail.com>
2018-12-09 12:17:09 +0000

    partials/templates

--- IntroToExpress/ExpressTemplates/app.js
+++ IntroToExpress/ExpressTemplates/app.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", (req, res) => {
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", (req, res) => {
@@ -16,7 +19,7 @@ app.get("/posts", (req, res) => {
             {title: "My adorable pet bunny", author: "Charlie"},
             {title: "Can you believe this pomsky", author: "Colt"},
         ];
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
