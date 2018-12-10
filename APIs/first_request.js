3b99c19f4b6c9d74bde72966df701f9c5ad41517
eddxw <eduardo.jemb@gmail.com>
2018-12-10 20:49:01 +0000

    Sunset in Hawaii Yahoo weather api request

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,21 +1,14 @@
-// const request = require('request');
-// request('http://www.google.com', function(error, response, body) {
-//     console.log('error:', error); // Print the error if one occurred
-//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//     console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
 const request = require("request");
-request('http://www.reddit.coms', (error, response, body) => {
-    if (error) {
-        console.log("Something went wrong!");
-        console.log(error);
-        console.log('statusCode:', response && response.statusCode);
-    }
-    else if (response.statusCode == 200) {
-        console.log(body);
+
+console.log("Sunset in Hawaii is at ...");
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        var parsedBody = JSON.parse(body);
+        console.log(parsedBody.query.results.channel.astronomy.sunset);
     }
     else {
+        console.log("Something went wrong!");
+        console.log(error);
         console.log('statusCode:', response && response.statusCode);
     }
 });
