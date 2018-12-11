dc6dcbd276461ed87a5fb6ae4e4ce26771ddb494
eddxw <eduardo.jemb@gmail.com>
2018-12-11 00:17:25 +0000

    query omdbapi

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+app.set("view engine", "ejs");
 
 app.get("/results", (req, res) => {
-    request("http://www.omdbapi.com/?apikey=thewdb&s=california", (error, response, strBody) => {
-        if(!error && response.statusCode == 200) {
-            let body = JSON.parse(strBody);
-            res.send(body.Search[0]);
+    var query = req.query.search;
+    const url = `http://www.omdbapi.com/?apikey=thewdb&s=${query}`;
+
+    request(url, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            let data = JSON.parse(body);
+            res.render("results", { data: data });
         }
     })
+});
+
+app.get("/", function(req, res) {
+    res.render("search");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
